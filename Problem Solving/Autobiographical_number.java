9b33615f63697021fa62cf6573b1fdd822a29148
Arun-1102 <arunganesan877@gmail.com>
2024-08-02 09:51:40 +0530

    new

---
+++ Problem Solving/Autobiographical_number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ class Autobiographical_number {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        String s=scan.nextLine();
+        char[] arr=s.toCharArray();
+      
+        StringBuffer str=new StringBuffer();
+           for(int i=0;i<s.length();i++){
+            int c=0;
+           
+            }
+
+           }
+
