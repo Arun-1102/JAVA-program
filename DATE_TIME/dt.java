85ab62919bccf977e911d0c3a2ffb85669dcfa85
Arun-1102 <arunganesan877@gmail.com>
2024-08-16 12:23:30 +0530

    date and time practice

---
+++ DATE_TIME/dt.java
@@ -0,0 +1,15 @@
+package DATE_TIME;
+import java.time.*;
+import java.time.Instant;
+public class dt{
+    public static void main(String [] args)
+    {
+        LocalDateTime k= LocalDateTime.now();
+        System.out.println(k);
+
+        
+        Instant t=Instant.now();
+        System.out.println(t);
+    }
+    
+}
